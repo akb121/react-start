bedae691a53f3f5f144fcdc8124ea39afa72c614
Ajay Brahma <ajaybrahma@Ajays-MacBook-Air.local>
2018-10-27 14:58:25 +0530

    added new changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'ajay'
+    }
+    this.changeName = this.changeName.bind(this);
+  }
+
+  changeName() {
+    let { name } = this.state;
+    name = (name === 'ajay') ? 'bidyut' : 'ajay';
+    this.setState({
+      name,
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <button onClick={this.changeName}>Change Name</button>
+        My name is {this.state.name}
       </div>
     );
   }
